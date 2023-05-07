e019555fb3d020801deb0f40f6faaec7b827167b
tranena <btranenaror9@gmail.com>
2023-05-07 12:38:48 +0800

    index post

--- public/index.js
+++ public/index.js
@@ -7,6 +7,25 @@ $(function () {
         byte.toString(16).padStart(2, "0")
     ).join("");
     $("#namespace_id").val(namespace_id)
-    console.log(namespace_id)
 
+
+    $('#form').submit(function (event) {
+        event.preventDefault();
+
+        $('#tx_result').html("executing...");
+
+        $.ajax({
+            url: '/',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (result) {
+                $('#tx_result').html("Submit Success");
+                const result_json = JSON.parse(result)
+                
+            },
+            error: function (error) {
+                $('#tx_result').html("Failed,Please make sure your node is available and try again!");
+            }
+        });
+    });
 });
